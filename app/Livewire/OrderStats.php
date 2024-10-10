c504446b5fb81f71c5b98293a8a4ed0b3eb430bf
Shahbaz <shahbaz.khan.wip@gmail.com>
2024-10-10 18:35:22 +0500

    fix copy format

--- app/Livewire/OrderStats.php
+++ app/Livewire/OrderStats.php
@@ -58,6 +58,8 @@ class OrderStats extends Component
         ", [$today, $currentMonth])->where('orders.status', '=', $this->orderStatus);
 
         if (Auth::user()->isSuperAdmin() || Auth::user()->isManager()) {
+
+            dump(Auth::user()->isManager());
             $stats = $stats->first();
         } else if (Auth::user()->isPMM()) {
             $stats = $stats->where('products.user_id', Auth::user()->id)->first();
