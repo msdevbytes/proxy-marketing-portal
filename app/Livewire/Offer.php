7acec93fac4c45f7b53f5eb34de72fda6312a42d
Shahbaz <shahbaz.khan.wip@gmail.com>
2024-12-06 14:39:54 +0500

    fixes

--- app/Livewire/Offer.php
+++ app/Livewire/Offer.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Offer as ModelsOffer;
 use Auth;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class Offer extends Component
@@ -13,7 +14,10 @@ class Offer extends Component
     public function mount()
     {
         // dd(Auth::user()->roles()->pluck('id')->toArray());
-        $this->offers = ModelsOffer::where('status', 1)->whereIn('for_user_role', Auth::user()->roles()->pluck('id')->toArray())->get();
+        $this->offers = ModelsOffer::where('status', 1)->whereIn('for_user_role', Auth::user()->roles()->pluck('id')->toArray())
+            ->whereDate('start_date', '<=', Carbon::now()->format('Y-m-d'))
+            ->whereDate('end_date', '>=', Carbon::now()->format('Y-m-d'))
+            ->get();
     }
     public function render()
     {
